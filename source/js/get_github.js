53b6b21e42f1564ef5ba3e5d8352c7bc2286d34a
Bruce Tan <brucetansh@gmail.com>
2018-01-23 05:36:18 +1300

    save metro

--- source/js/get_github.js
+++ source/js/get_github.js
@@ -1,25 +1,46 @@
 
 
-function getGithubStars(user, limit, callback){
 
+function getGithubStars(user, limit, callback){
     var thresh = 1
     var per_page = 100
     var page = 1
     var repos = []
     var res_count = 0
+    var github_url = 'https://api.github.com/graphql'
+    var query =  'query{\
+        user(login: "' + user + '") {\
+          name\
+           repositories(first: 100, privacy: PUBLIC) {\
+             nodes {\
+                   name\
+                   url\
+                    stargazers(){\
+                        totalCount\
+                    }\
+                    primaryLanguage(){\
+                              name\
+                    }\
+                    description\
+             }\
+                totalCount\
+           }\
+         }\
+      }'
+      
 
-    request( geturl(user, page) , handlecb)
+    graphql_request( github_url ,query,  handlecb)
 
     function handlecb(res){
         if (!res) {
-            console.log(res.message)
+            console.error(res)
             return
         }
         res_count = res.length
         repos = repos.concat(res)
 
         if(res_count == per_page){
-            request(geturl( user, ++page), handlecb)
+            graphql_request(github_url, query, handlecb)
         }else{
             output(repos)
         }
@@ -29,34 +50,54 @@ function getGithubStars(user, limit, callback){
         return 'https://api.github.com' + '/users/' + user + '/repos?per_page=' + per_page + '&page=' + i.toString()
     }
 
-    function request (mypath, cb) {
+    function graphql_request (url, query, cb) {
         var body = ''
         var result = ''
-        url =  mypath
-        console.log('request', url)
-        $.when( $.ajax(url) ).then(function(data, textStatus, jqXHR){
-            console.log(data)
+        // graph(`query :{ user(login: "halfer53") {name} }`, {declare: true})
+        // .then(function(data){
+        //     data = data['data']
+        //     console.log(data)
+        //     cb(data)
+        // })
+        // .catch(function(err){
+        //     console.error(err)
+        // })
+        $.when( $.ajax({
+            url: url,
+            data: JSON.stringify({"query":query,"variables":"{}"}),
+            headers: {
+                Authorization: "Bearer 9b1b997d58240f255469cc413cb7bcedb8fd4e59"
+              },
+            contentType: "application/json",
+            method: "POST",
+            async: true,
+            dataType: "json",
+        })).then(function(raw_data, textStatus, jqXHR){
+            data = raw_data['data']
+            if(!data){
+                error = data['errors'][0]
+                console.error(error.message)
+            }
             cb(data)
         }, function(data, textStatus, jqXHR){
             console.log(textStatus, data)
         })
     }
     
-    function output (repos) {
+    function output (graphql_raw_res) {
         var total = 0,
             longest = 0,
+            repos = graphql_raw_res[0].user.repositories.nodes
+            console.log(graphql_raw_res[0].user.repositories.totalCount)
             list = repos
                 .filter(function (r) {
-                    total += r.stargazers_count
-                    if (r.stargazers_count >= thresh) {
-                        if (r.name.length > longest) {
-                            longest = r.name.length
-                        }
+                    total += r.stargazers.totalCount
+                    if (r.stargazers.totalCount >= thresh) {
                         return true
                     }
                 })
                 .sort(function (a, b) {
-                    return b.stargazers_count - a.stargazers_count
+                    return b.stargazers.totalCount - a.stargazers.totalCount
                 })
 
         if (list.length > limit) {
@@ -73,8 +114,8 @@ function getGithubStars(user, limit, callback){
 getGithubStars(github_username, github_threshhold, function(list){
     var val = list.map( r => {
         currval =   '<li>' + 
-                    '<a href="' + r.html_url + '">' + r.name + '</a>' + 
-                    '<span>' + r.language + ', ' + r.stargazers_count + ' star ' + '</span>' + 
+                    '<a href="' + r.url + '">' + r.name + '</a>' + 
+                    '<span>' + r.primaryLanguage.name+ ', ' + r.stargazers.totalCount + ' star ' + '</span>' + 
                     '<br>' + r.description + 
                     '</li>'
         // console.log('currval', currval)
